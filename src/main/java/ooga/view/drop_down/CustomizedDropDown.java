a6847b6e12762c292f242fdc06533ea13405e553
Leila Nelson <lmn25@duke.edu>
2022-11-12 13:12:42 -0500

    Created foundational abstract class for our drop down menus

---
+++ src/main/java/ooga/view/drop_down/CustomizedDropDown.java
@@ -0,0 +1,39 @@
+package ooga.view.drop_down;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ResourceBundle;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import ooga.Main;
+import ooga.view.InteractiveObject;
+import ooga.view.View;
+
+public abstract class CustomizedDropDown extends VBox implements InteractiveObject {
+  private ChoiceBox<String> choiceBox;
+  private final ResourceBundle myResources;
+
+  public CustomizedDropDown (String labelKey, String language)  {
+    myResources = ResourceBundle.getBundle(Main.DEFAULT_LANGUAGE_PACKAGE + language);
+    this.getChildren().add(labelDropDown(labelKey));
+  }
+
+  private Label labelDropDown (String key) {
+    return new Label(myResources.getString(key));
+  }
+
+  public abstract void createChoices();
+
+  @Override
+  public void setAction(Method method, View view) {
+    choiceBox.getSelectionModel().selectedIndexProperty().addListener(
+        (observable, oldValue, newValue) -> {
+          try {
+            method.invoke(view);
+          } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+          }
+        });
+  }
+}
