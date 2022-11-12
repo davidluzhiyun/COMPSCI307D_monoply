ffe892de28976f7dab4533e550b6a29525df3b09
Luyao Wang <lw337@duke.edu>
2022-11-12 12:36:36 -0500

    starting implementing model

---
+++ doc/plan/api/model/ConcreteModel.java
@@ -0,0 +1,38 @@
+package model.place;
+
+public class Model implements model.Model {
+  @Override
+  public Object getCommandArgs() {
+    return null;
+  }
+
+  @Override
+  public void publishDice() {
+
+  }
+
+  @Override
+  public void currentPlayer() {
+
+  }
+
+  @Override
+  public void playersData() {
+
+  }
+
+  @Override
+  public void boardData() {
+
+  }
+
+  @Override
+  public void stationaryAction() {
+
+  }
+
+  @Override
+  public void boardUpdateData() {
+
+  }
+}
