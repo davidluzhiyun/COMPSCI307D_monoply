88d8f0a27e71e07c233413f6136f92ace86396de
Leila Nelson <lmn25@duke.edu>
2022-11-12 00:10:10 -0500

    created resources directory, started to make abstract class View

---
+++ src/main/java/ooga/view/View.java
@@ -0,0 +1,18 @@
+package ooga.view;
+
+import javafx.scene.Scene;
+
+public abstract class View {
+
+  public static final String DEFAULT_RESOURCE_PACKAGE = "ooga/properties.";
+  public static final String DEFAULT_STYLE_PACKAGE = "ooga/style.";
+  public static final String STYLESHEETS = "/" + DEFAULT_STYLE_PACKAGE.replace(".", "/");
+
+  public abstract Object makeButtonObject();
+
+  public void styleScene(Scene scene, String file) {
+    scene.getStylesheets()
+        .add(View.class.getResource(STYLESHEETS + file).toExternalForm());
+//    myStage.setScene(scene);
+  }
+}
