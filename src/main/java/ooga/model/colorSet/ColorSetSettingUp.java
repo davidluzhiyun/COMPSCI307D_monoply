9b0ea2b5a69d447a4b7a5674e59deb12896509e3
Zhiyun (David) Lu <zl303@duke.edu>
2022-11-13 16:26:21 -0500

    Classes for color set in progress

---
+++ src/main/java/ooga/model/colorSet/ColorSetSettingUp.java
@@ -0,0 +1,5 @@
+package ooga.model.colorSet;
+
+public interface ColorSetSettingUp {
+
+}
---
+++ src/main/java/ooga/model/colorSet/ConcreteColorSet.java
@@ -0,0 +1,10 @@
+package ooga.model.colorSet;
+
+/**
+ * Concrete class for managing color sets of streets
+ * Used to check relationship between a collection of streets and color sets
+ * For future used add in more methods
+ */
+public class ConcreteColorSet {
+
+}
