266f43eecf29f3e26d754e43eda58fb9e2b5d8e1
Luyao Wang <lw337@duke.edu>
2022-12-12 15:03:15 -0500

    throw exception if bad gama data

---
+++ src/main/java/ooga/model/exception/BadDataException.java
@@ -0,0 +1,11 @@
+package ooga.model.exception;
+
+public class BadDataException extends MonopolyException {
+  public BadDataException() {
+    super();
+  }
+  public BadDataException(String message) {
+    super(message);
+  }
+
+}
