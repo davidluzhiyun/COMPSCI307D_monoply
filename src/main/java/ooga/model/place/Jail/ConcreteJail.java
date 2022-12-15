e9ea3df2a5140da031047b4ba552695c372992d2
Luyao Wang <lw337@duke.edu>
2022-12-14 20:57:32 -0500

    the previous fix did not really fix it because I changed back the fix to reproduce the bug

--- src/main/java/ooga/model/place/Jail/ConcreteJail.java
+++ src/main/java/ooga/model/place/Jail/ConcreteJail.java
@@ -1,6 +1,7 @@
 package ooga.model.place.Jail;
 
 import java.util.Collection;
+
 import ooga.event.GameEventHandler;
 import ooga.model.player.Player;
 import ooga.model.StationaryAction;
@@ -8,11 +9,13 @@ import ooga.model.place.AbstractPlace;
 
 public class ConcreteJail extends AbstractPlace implements Jail {
 
-  public ConcreteJail(String id) {
-    super(id, new GameEventHandler());
+  public ConcreteJail(String id, GameEventHandler gameEventHandler) {
+    super(id, gameEventHandler);
   }
+
   /**
    * Overrides parent to output the StationaryActions for players in jail and just parking
+   *
    * @param player the current player
    * @return
    * @author David Lu
@@ -20,7 +23,7 @@ public class ConcreteJail extends AbstractPlace implements Jail {
   @Override
   public Collection<StationaryAction> getStationaryActions(Player player) {
     Collection<StationaryAction> stationaryActionList = getCommonTurnBasedStationaryAction(player);
-    if (stationaryActionList.contains(StationaryAction.ROLL_DICE )&& (player.remainingJailTurns() > 0)){
+    if (stationaryActionList.contains(StationaryAction.ROLL_DICE) && (player.remainingJailTurns() > 0)) {
       stationaryActionList.add(StationaryAction.PAY_FINE);
     }
     return stationaryActionList;
